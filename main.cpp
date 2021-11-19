9a37ce3a37810050bfa297e6e91d53ff52b8c63f
mukunthan7 <vijimurugesan7@gmail.com>
2021-11-19 19:47:29 +0530

    basic sum of c++

---
+++ main.cpp
@@ -0,0 +1,11 @@
+// add are two numbers cpp
+#include <iostream>
+using namespace std;
+main()
+{
+    int a, b, c;
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+    c = a + b;
+    cout << "the sum of twonumber is "<< c << endl;
+}
