d3d494f60b45cef3afa6c63e6dd81a123e5e7cc0
mukunthan7 <vijimurugesan7@gmail.com>
2021-11-19 19:52:29 +0530

    for loop in c

---
+++ for.c
@@ -0,0 +1,13 @@
+//for loop in c 
+#include<stdio.h>
+void main()
+{
+    int i;
+    char c;
+    printf("Enter a character:\n");
+    scanf("%c",&c);
+    for(i=0;i<3;i++)
+    {  
+        printf("%c\n",c);
+    }
+}
