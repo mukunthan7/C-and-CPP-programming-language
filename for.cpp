3d72be9a63577a760db63914b3bb10a32f33c85f
mukunthan7 <vijimurugesan7@gmail.com>
2021-11-19 19:55:10 +0530

    for loop in c++

---
+++ for.cpp
@@ -0,0 +1,14 @@
+// for loop in cpp
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i; 
+    char c;
+    cout<<"Enter a character: ";
+    cin>>c;
+    for(i=0;i<3;i++)
+    {
+        cout<<"You entered: "<<c<<endl;
+    } 
+}
