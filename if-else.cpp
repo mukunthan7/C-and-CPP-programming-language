fdd0539f3ad7b054d79c47f71b60e97c56fb8a81
mukunthan7 <vijimurugesan7@gmail.com>
2021-11-22 15:30:09 +0530

    if-else in c++

---
+++ if-else.cpp
@@ -0,0 +1,14 @@
+// if else statement cpp
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cout<<"Enter two numbers: ";
+    cin>>a>>b;
+    if(a>b)
+        cout<<"a is greater than b";
+    else
+        cout<<"b is greater than a";
+    return 0;
+}
